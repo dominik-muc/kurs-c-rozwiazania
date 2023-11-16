6becd7bd17f51ac1787a3dfaa0d0ab29b4770d37
Dominik Muc <dominik.m.muc@gmail.com>
2023-11-16 09:20:07 +0100

    Poprawiono zadanie 3 z listy 4 10/10

--- Lista 4/3.c
+++ Lista 4/3.c
@@ -3,50 +3,39 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct Fraction{
-    int numerator;
-    int denominator;
-};
-
-void reduce_fraction(struct Fraction* fraction);
-void add_fraction(struct Fraction* result, int numerator, int denominator);
-void read_square(struct Fraction* result, int layer);
+void read_square(int* result, int* max_layer, int layer);
 
 int main(){
-    struct Fraction result;
-    result.numerator = 0;
-    result.denominator = 1;
-    read_square(&result, 0);
-    printf("%i/%i", result.numerator, result.denominator);
+    int result[64] = {0};
+    int max_layer = 0;
+    read_square(result, &max_layer, 0);
+    unsigned long long numerator = 0;
+    unsigned long long denominator = 1;
+
+    for(int i = 0; i <= max_layer; i++){
+        numerator <<= 2;
+        numerator += result[i];
+    }
+
+    if(numerator > 0) denominator <<= 2*(max_layer);
+
+    while(numerator % 2 == 0 && numerator > 0){
+        numerator /= 2;
+        denominator /= 2;
+    }
+    
+    printf("%llu/%llu", numerator, denominator);
     return 0;
 }
 
-
-void reduce_fraction(struct Fraction* fraction){
-    int i = 2;
-    while(i <= fraction->numerator){
-        while((fraction->numerator % i == 0) && (fraction->denominator % i == 0)){
-            fraction->numerator /= i;
-            fraction->denominator /= i;
-        }
-        i++;
-    }
-}
-
-void add_fraction(struct Fraction* result, int numerator, int denominator){
-    result->numerator *= denominator;
-    result->numerator += numerator * result->denominator;
-    result->denominator *= denominator;
-    reduce_fraction(result);
-}
-
-void read_square(struct Fraction* result, int layer){
+void read_square(int* result, int* max_layer, int layer){
     int c;
     scanf("%i", &c);
-    if(c == 1) add_fraction(result, 1, pow(4, layer));
+    if(c == 1) result[layer]++;
     if(c == 2){
+        if(*max_layer == layer) (*max_layer)++;
         for(int i = 0; i < 4; i++){
-            read_square(result, layer + 1);
+            read_square(result, max_layer, layer + 1);
         }
     }
 }
