e3b07ebef7199178fa7ae102250428433a13a58d
Dominik Muc <dominik.m.muc@gmail.com>
2023-11-25 19:55:28 +0100

    Skonczono zadanie 1 z listy 6

--- Lista 6/1.c
+++ Lista 6/1.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 int write_divison(long divident, long divisor);
+int len(long number);
 
 int main(int argc, char** argv){
     if(argc != 3){
@@ -18,13 +19,58 @@ int main(int argc, char** argv){
         fprintf(stderr, "Niepoprawne argumenty!\n");
         return 0xFF;
     }
-
     if(write_divison(divident, divisor)){
         fprintf(stderr, "Wystąpił błąd dzielenia. Program zakończył działanie.\n");
         return 0xFF;
     }
     return 0;
 }
+
+/*
+ * Co z tego że C nie ma klas???
+ * Klasa Number:
+*/
+typedef struct Number{
+    //wyobraźmy sobie, że to jest prywatne...
+    char* ascii;
+    long dec;
+    int iterator;
+    int len;
+} Number;
+
+Number* Number_init(long n){
+    Number* number = malloc(sizeof(Number));
+    number->ascii = malloc(len(n));
+    sprintf(number->ascii, "%li", n);
+    number->dec = n;
+    number->iterator = 0;
+    number->len = len(n);
+    return number;
+}
+
+void Number_destroy(Number* self){
+    free(self->ascii);
+    free(self);
+}
+
+int Number_get_next_digit(Number* self){
+    if(self->iterator >= self->len) return -1;
+    return (int)(self->ascii[self->iterator++]) - 48;
+}
+
+int Number_decrement(Number* self){
+    if(self->iterator == 0) return -1;
+    return --self->iterator;
+}
+
+int Number_get_len(Number* self){
+    return self->len;
+}
+
+int Number_get_current_iterator(Number* self){
+    return self->iterator;
+}
+/* Koniec klasy */
 
 int len(long number){
     if(number == 0) return 1;
@@ -37,60 +83,74 @@ void print_spaces(int n){
 
 void print_lines(int n){
     for(int i = 0; i < n; i++) putchar('-');
+    putchar('\n');
 }
+
+//kolejna rzecz której nie ma w c bez żadnego powodu:
+#define bool __int8_t
+#define true 1
+#define false 0
 
 int write_divison(long divident, long divisor){
     if(divisor == 0) return 0xFF;
     long result = divident / divisor;
-
-    char* number = malloc(len(divident));
-    sprintf(number, "%li", divident);
+    Number* number = Number_init(divident);
     
-    print_spaces(len(divident) - len(result) + 1);
+    print_spaces(Number_get_len(number) - len(result) + 1);
     printf("%li\n ", result);
-    print_lines(len(divident));
-    putchar('\n');
+    print_lines(Number_get_len(number));
     printf(" %li : %li\n", divident, divisor);
 
     if(divident < divisor){
         printf("-0\n -\n %li\n", divident);
         return 0;
     }
-
+    if(divident == divisor){
+        printf("-%li\n -\n 0\n", divident);
+        return 0;
+    }
+    
     int current_divident = 0;
-    int iterator = 0;
-    int spaces = 0;
-    int first = 1;
-    int remainder = 0;
-    int skipped = 0;
-    while(iterator <= len(divident)-1 ){
-        while(current_divident < divisor && iterator <= len(divident)-skipped){
+    int sub = 0;
+    int digit = 0;
+    bool first = 1;
+
+    while((digit = Number_get_next_digit(number)) != -1){
+        
+        if(current_divident < divisor){
             current_divident *= 10;
-            current_divident += (int)number[iterator] - 48;
-            iterator++;
-            spaces++;
+            current_divident += digit;
+            continue;
         }
         if(!first){
-            print_spaces(spaces);
+            print_spaces(Number_get_current_iterator(number) - len(current_divident));
             printf("%i\n", current_divident);
         }
-
-        remainder = (current_divident / divisor ) * divisor;
-
-        print_spaces(spaces-1);
-        printf("-%i\n", remainder);
-        print_spaces(spaces);
-        print_lines(len(remainder));
-
+        else first = false;
+        
+        sub = (current_divident / divisor) * divisor;
+        print_spaces(Number_get_current_iterator(number) - len(sub) - 1);
+        printf("-%i\n", sub);
+        print_spaces(Number_get_current_iterator(number) - len(sub));
+        print_lines(len(sub));
         current_divident %= divisor;
-        if(current_divident == 0) current_divident = -1;
-        if(first){
-            spaces = 1;
-            first = 0;
-        }
-        getchar();
+        Number_decrement(number);
     }
-    print_spaces(spaces+len(remainder)-len(current_divident));
+    print_spaces(Number_get_current_iterator(number) - len(current_divident) + 1);
     printf("%i\n", current_divident);
+    sub = (current_divident / divisor) * divisor;
+    print_spaces(Number_get_current_iterator(number) - len(sub));
+    printf("-%i\n", sub);
+    print_spaces(Number_get_current_iterator(number) - len(sub) + 1);
+    print_lines(len(sub));
+    current_divident %= divisor;
+
+    //^ próbowałem to wrzucić do pętli ale nieważne jak próbowałem nie udało mi się
+
+    print_spaces(Number_get_current_iterator(number) - len(current_divident) + 1);
+    printf("%i\n", current_divident);
+
+    Number_destroy(number);
+    number = NULL;
     return 0;
 }
